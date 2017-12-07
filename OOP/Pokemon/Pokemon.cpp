73f74f0bb57dfb952be14d9c04328a646d2bdbeb
Amit Bernstein <amitbern88@gmail.com>
2017-12-07 16:45:14 +0200

    avsha efes

---
+++ OOP/Pokemon/Pokemon.cpp
@@ -0,0 +1,23 @@
+#include "Pokemon.h"
+
+
+
+void Pokemon::pokBattle()
+{
+}
+
+void Pokemon::mPunch()
+{
+	//This method should decrease 0.5xp to the enemy Pokemon
+	return ;
+}
+
+Pokemon::Pokemon()
+{
+	mXp = 20;
+}
+
+
+Pokemon::~Pokemon()
+{
+}
