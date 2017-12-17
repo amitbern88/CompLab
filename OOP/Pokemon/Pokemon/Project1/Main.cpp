bc1809b06ea63e6c79e9a7a5bb921ac686b1675d
nimrodko <nimrodkor@gmail.com>
2017-12-17 14:31:57 +0300

    Add timing to main

--- OOP/Pokemon/Pokemon/Project1/Main.cpp
+++ OOP/Pokemon/Pokemon/Project1/Main.cpp
@@ -1,11 +1,18 @@
-#include "Game.h"
 #include <iostream>
+#include <chrono>
+#include "Game.h"
 
 using namespace std;
 int main()
 {
+	auto start = std::chrono::high_resolution_clock::now();
 	Game game = Game();
 	game.setup();
 	game.start();
+	auto finish = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> elapsed = finish - start;
+
+	cout << "Elapsed time: " << elapsed.count() << endl;
 	return 0;
 }
